733544c906e451b0e49d6b2c481b21b6e29aa19c
Tobias de Groen <tdgroen@lias-software.nl>
2017-02-01 15:23:32 +0100

    change(prototype): include Array extensions within prototype.ts

--- lib/prototype.ts
+++ lib/prototype.ts
@@ -1,6 +1,18 @@
-/// <reference path="./prototype.d.ts" />
-
 import * as lib from './index';
+
+declare global {
+  interface Array<T> {
+    qContains<TR>(r: TR[], predicate?: (l: T, r: TR) => boolean): boolean;
+    qIntersect<TR>(r: TR[], predicate?: (l: T, r: TR) => boolean): T[];
+    qSame<TR>(r: TR[], predicate?: (l: T, r: TR) => boolean): boolean;
+    qEmpty(): boolean;
+    qFirst(predicate?: (l: T) => boolean): T;
+    qLast(predicate?: (l: T) => boolean): T;
+    qRotate(offset: number): T[];
+    qMapMany<TR>(selector?: (l: T) => TR[]): TR[];
+    qDistinct(key?: (l: T) => any): T[];
+  }
+}
 
 if (!Array.prototype.qContains) {
     Array.prototype.qContains = function(r, predicate?) {
