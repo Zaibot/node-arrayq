b9fdf90b1b9c9b72728bd06e515996686cdaec1f
Tobias de Groen <tdgroen@lias-software.nl>
2017-03-01 10:29:58 +0100

    refactor(mapMany): rewrite without Array

--- lib/mapMany.ts
+++ lib/mapMany.ts
@@ -1,10 +1,29 @@
 import { ProjectMany } from './types';
 
 export default function mapMany<TL, TR>(l: TL[], selector?: ProjectMany<TL, TR>): TR[] {
-  if (selector) {
-    const lists = l.map(selector);
-    return Array.prototype.concat.apply([], lists);
-  } else {
-    return Array.prototype.concat.apply([], l);
-  }
+    if (selector) {
+        const ii = l.length;
+        var res = Array();
+        var count = 0;
+        for (let i = 0; i < ii; i++) {
+            const deep = selector(l[i]);
+            if (deep === undefined) continue;
+
+            for (let j = 0, jj = deep.length; j < jj; j++) {
+                res[count++] = deep[j];
+            }
+        }
+        return res;
+    } else {
+        const ii = l.length;
+        var res = Array();
+        var count = 0;
+        for (let i = 0; i < ii; i++) {
+            const deep = l[i] as any as TR[];
+            for (let j = 0, jj = deep.length; j < jj; j++) {
+                res[count++] = deep[j];
+            }
+        }
+        return res;
+    }
 }
