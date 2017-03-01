e5669db32e3be1414c3fb2b53199c01e15fdceb8
Tobias de Groen <tdgroen@lias-software.nl>
2017-03-01 14:31:45 +0100

    change(benchmark): add concat to qSelectMany

--- benchmark/qselectmany.js
+++ benchmark/qselectmany.js
@@ -5,5 +5,7 @@ const links = data.qSelect(x => x._links);
 
 module.exports = () =>
   new Suite()
+    .add('concat()', () => Array.prototype.concat.apply([], links))
+    .add('concat(map)', () => Array.prototype.concat.apply([], data.map(x => x._links)))
     .add('qMapMany()', () => links.qMapMany())
     .add('qMapMany(_links)', () => data.qMapMany(x => x._links));
