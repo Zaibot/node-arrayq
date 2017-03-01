72f164fa128e75443dfe18280d615b4afadc0257
Tobias de Groen <tdgroen@lias-software.nl>
2017-03-01 14:32:08 +0100

    refactor(benchmark): alternate list of suites

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,6 +1,15 @@
-['jslength','jsmap','qselectmany','jssome-pes','jssome-op','jsevery-pes','jsevery-op'].forEach(suite => {
-  console.log(`[${suite}]`);
-  require(`./${suite}`)()
+[
+  ['jslength', require('./jslength')],
+  ['jsmap', require('./jsmap')],
+  ['qselectmany', require('./qselectmany')],
+  ['jssome-pes', require('./jssome-pes')],
+  ['jssome-op', require('./jssome-op')],
+  ['jsevery-pes', require('./jsevery-pes')],
+  ['jsevery-op', require('./jsevery-op')]
+].forEach(reg => {
+  const name = reg[0], suiteFactory = reg[1];
+  console.log(`[${name}]`);
+  suiteFactory()
     .on('cycle', (event) => console.log(String(event.target)))
     .on('error', (event) => console.error(event.target.error))
     .on('complete', function(event) {
