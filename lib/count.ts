366d9ccc0d5aecc0e1e7d1291dfbf9a26ce45414
Tobias de Groen <tdgroen@lias-software.nl>
2017-03-02 10:27:10 +0100

    fix(count): predicate optional

--- lib/count.ts
+++ lib/count.ts
@@ -1,9 +1,13 @@
 import { ItemPredicate } from './types';
 
-export default function count<T>(items: T[], predicate: ItemPredicate<T>): number {
+export default function count<T>(items: T[], predicate?: ItemPredicate<T>): number {
+  if (predicate) {
     let count = 0;
     for (var i = 0, ii = items.length; i < ii; i++) {
-        count = count + (predicate(items[i], i, items) ? 1 : 0);
+      count = count + (predicate(items[i], i, items) ? 1 : 0);
     }
     return count;
+  } else {
+    return items.length;
+  }
 }
